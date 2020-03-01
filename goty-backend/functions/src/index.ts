d43f0ebdda86388be51c6388f8489864c7e9d9e9
Pedro <castro.fcefyn@gmail.com>
2020-03-01 13:21:52 -0300

    169. Firebase: Credenciales paratrabajr local y remotamente

--- goty-backend/functions/src/index.ts
+++ goty-backend/functions/src/index.ts
@@ -1,4 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://firestore-grafica-f5c68.firebaseio.com"
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
